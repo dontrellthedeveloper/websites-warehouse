fb85fb4fe704a74ec6cd6637d085c19ffca73d95
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-12-31 12:49:57 -0800

    huge commit: added multiple js plugins to js folder. Trying different plugins for portfolio. Decided to use custom thumbnail slider.

--- js/script.js
+++ js/script.js
@@ -6,3 +6,61 @@ $(window).on('load', function () {
     $('#preloader').delay(350).fadeOut('slow');
 });
 
+/* ============================================
+|  |  |  |  Tilted Scroll
+============================================ */
+
+$(".websites-portfolio").tiltedpage_scroll({
+    sectionContainer: "> section",
+    angle: 30,
+    opacity: true,
+    scale: true,
+    outAnimation: true
+});
+
+/* ============================================
+|  |  |  |  Magnific popup
+============================================ */
+
+$(document).ready(function() {
+    $('.port-popup').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+});
+
+/* ============================================
+|  |  |  |  Tilted Scroll
+============================================ */
+
+$(".websites-portfolio").tiltedpage_scroll({
+    sectionContainer: "> section",
+    angle: 50,
+    opacity: true,
+    scale: true,
+    outAnimation: true
+});
+
+/* ================================================
+|   |   |   |   Responsive Tabs
+================================================ */
+$(function () {
+    $("#services-tabs").responsiveTabs({
+        animation: 'slide'
+    })
+});
+
+
+$(function () {
+    $("#portfolio-tabs").responsiveTabs({
+        animation: 'slide'
+    })
+});
+
+
+
+
+
