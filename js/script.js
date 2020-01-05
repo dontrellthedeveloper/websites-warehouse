5238099d5de2f5a7ea3a98b0a6a72fb2b01045ea
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-01-04 18:58:21 -0800

    added base tools section

--- js/script.js
+++ js/script.js
@@ -109,6 +109,18 @@ $(function() {
 });
 
 
+$(function() {
+    $("#testimonial-slider").owlCarousel({
+        items: 1,
+        autoplay: false,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+    });
+});
 
 /* ================================================
 |   |   |   |   Page Scroll
