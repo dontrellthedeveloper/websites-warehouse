8904d978b993d30a39c35c98f340fa836787053b
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-02-09 03:05:41 -0800

    added debugger

--- mgs-functions.php
+++ mgs-functions.php
@@ -20,7 +20,7 @@
 		// 0 = off (for production use)
 		// 1 = client messages
 		// 2 = client and server messages
-		$mail->SMTPDebug = 0;
+		$mail->SMTPDebug = 2;
 		//Ask for HTML-friendly debug output
 		$mail->Debugoutput = 'html';
 		//Set the hostname of the mail server (Change here)
