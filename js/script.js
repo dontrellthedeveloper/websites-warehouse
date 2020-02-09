bcc6d7542ba6e61d6c2c0982b3d2794d8e841854
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-02-09 02:18:53 -0800

    made adjustments to responsive design

--- js/script.js
+++ js/script.js
@@ -274,7 +274,22 @@ $(function () {
 
 });
 
+/* ================================================
+|   |   |   |   Mobile Menu
+================================================ */
 
+$(function() {
+
+    // Show mobile nav
+    $("#mobile-nav-open-btn").click(function() {
+        $("#mobile-nav").css("height", "100%");
+    });
+
+    // Hide mobile nav
+    $("#mobile-nav-close-btn, #mobile-nav a").click(function() {
+        $("#mobile-nav").css("height", "0%");
+    });
+});
 
 
 
