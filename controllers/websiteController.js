bd2751590e966542d16ddb3194684f37bc04f16e
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-11-13 14:01:14 -0800

    improved api by implementing pagination

--- controllers/websiteController.js
+++ controllers/websiteController.js
@@ -34,6 +34,21 @@ exports.getAllWebsites = async (req, res) => {
             query = query.select('-_v');
         }
 
+
+
+        // 4) Pagination
+        const page = req.query.page * 1 || 1;
+        const limit = req.query.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+
+        query = query.skip(skip).limit(limit);
+
+        if (req.query.page) {
+            const numWebsites = await Website.countDocuments();
+            if (skip >= numWebsites) throw new Error('This page does not exist');
+        }
+
+
         // EXECUTE QUERY
         const websites = await query;
 
