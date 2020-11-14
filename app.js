9be084d5e898e7c4b9d553ca5a5696ce8ce6c9b0
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-11-13 17:56:24 -0800

    handling unhandled routes, refactored errors and implemented global error handling middleware

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const morgan = require('morgan');
 
 const app = express();
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const websiteRouter = require('./routes/websiteRoutes');
+const userRouter = require('./routes/userRoutes');
 
 // 1. Middleware
 if (process.env.NODE_ENV === 'development') {
@@ -13,16 +17,20 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 
-// 2. Route Handlers
-const websiteRouter = require('./routes/websiteRoutes');
-const userRouter = require('./routes/userRoutes');
 
-
-
-// 3. Routes
+// 2. Routes
 
 app.use('/api/v1/websites', websiteRouter);
 app.use('/api/v1/users', userRouter);
+
+
+app.all('*', (req,res,next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+
 
 
 module.exports = app;
