d835235c716f58e1cb098031a7b679b8d3473fca
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-11-13 16:28:51 -0800

    refactored API. Places queries inside a utils folder.

--- controllers/websiteController.js
+++ controllers/websiteController.js
@@ -1,5 +1,5 @@
 const Website = require('../models/websitesModel');
-
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.aliasTopWebsites = (req, res, next) => {
     req.query.limit = '5';
@@ -10,53 +10,13 @@ exports.aliasTopWebsites = (req, res, next) => {
 
 exports.getAllWebsites = async (req, res) => {
     try {
-        // BUILD QUERY
-        // 1A) Filtering
-        const queryObj = {...req.query};
-        const excludedFields = ['page', 'sort', 'limit', 'fields'];
-        excludedFields.forEach(el => delete queryObj[el]);
-
-        // 1B) Advanced Filtering
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-        console.log(JSON.parse(queryStr));
-
-        let query = Website.find(JSON.parse(queryStr));
-
-        // 2) Sorting
-        if(req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' ');
-            console.log(sortBy);
-            query = query.sort(sortBy);
-        } else {
-            query = query.sort('-createdAt');
-        }
-
-        // 3) Field limiting
-        if (req.query.fields) {
-            const fields = req.query.fields.split(',').join(' ');
-            query = query.select(fields);
-        } else {
-            query = query.select('-_v');
-        }
-
-
-
-        // 4) Pagination
-        const page = req.query.page * 1 || 1;
-        const limit = req.query.limit * 1 || 100;
-        const skip = (page - 1) * limit;
-
-        query = query.skip(skip).limit(limit);
-
-        if (req.query.page) {
-            const numWebsites = await Website.countDocuments();
-            if (skip >= numWebsites) throw new Error('This page does not exist');
-        }
-
-
         // EXECUTE QUERY
-        const websites = await query;
+        const features = new APIFeatures(Website.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate();
+        const websites = await features.query;
 
         // SEND RESPONSE
         res.status(200).json({
