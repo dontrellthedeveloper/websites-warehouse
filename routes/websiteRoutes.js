0d90ff023d3542855e9c0bb57322e1d670490ff8
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-11-14 23:35:28 -0800

    added protected routes for user login. Users must have the correct jwt in their header to be authenticated to use protected routes

--- routes/websiteRoutes.js
+++ routes/websiteRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const websiteController = require('./../controllers/websiteController');
+const authController = require('./../controllers/authController');
+
 
 const router = express.Router();
 
@@ -10,7 +12,7 @@ router
 
 router
     .route('/')
-    .get(websiteController.getAllWebsites)
+    .get(authController.protect, websiteController.getAllWebsites)
     .post(websiteController.createWebsite);
 
 router
