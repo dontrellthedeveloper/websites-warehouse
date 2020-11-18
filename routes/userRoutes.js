726a73a6a5e79a5fa05e66cb3e6095196cd19242
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-11-18 01:01:14 -0800

    added a /me endpoint to get current user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,6 +12,12 @@ router.patch('/resetPassword/:token', authController.resetPassword);
 
 router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
 
+router.get('/me',
+    authController.protect,
+    userController.getMe,
+    userController.getUser
+);
+
 router.patch('/updateMe', authController.protect, userController.updateMe);
 router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
