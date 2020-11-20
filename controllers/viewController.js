e101ddb7e2b34d0ce95f59cef94786db13799a17
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-11-19 20:28:44 -0800

    started to dynamically render the homepage by adding the Website model to render content

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,24 @@
-exports.getOverview = (req,res) => {
+
+const Website = require('../models/websitesModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req,res, next) => {
+
+    const businessWebsites = await Website.find({"websiteCategory":"business"});
+    const clothingWebsites = await Website.find({"websiteCategory":"clothing"});
+    const healthWebsites = await Website.find({"websiteCategory":"beauty/health"});
+    const designWebsites = await Website.find({"websiteCategory":"design/event"});
+
+
+
     res.status(200).render('overview', {
-        title: 'All Websites'
+        title: 'All Websites',
+        businessWebsites,
+        clothingWebsites,
+        healthWebsites,
+        designWebsites,
     });
-};
+});
 
 exports.getWebsite = (req,res) => {
     res.status(200).render('website', {
