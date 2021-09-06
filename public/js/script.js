c35ebc5ff25d0a2d9fa7cea43125c505c4eb7a31
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-09-06 06:07:08 -0400

    added pricing section

--- public/js/script.js
+++ public/js/script.js
@@ -314,6 +314,15 @@ $(function () {
         type: 'image',
         gallery: {
             enabled: true
+        },
+        image: {
+            titleSrc: function (item) {
+                var markup = '';
+                if (item.el[0].hasAttribute("data-title")) {
+                    markup += '<a>' + item.el.attr('data-title') + '</a>';
+                }
+                return markup
+            }
         }
     });
 
